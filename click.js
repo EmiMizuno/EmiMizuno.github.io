7c8184e1485f208757ca92d9638285405cd85262
EmiMizuno <117380153+EmiMizuno@users.noreply.github.com>
2022-11-03 13:22:28 -0300

    Add files via upload

---
+++ click.js
@@ -0,0 +1,10 @@
+function umClique(){
+    document.getElementById("imagem").src="kind.png";
+    
+  }
+  
+  
+  function doisCliques(){
+    document.getElementById("imagem").src="Piu Piu.png";
+    
+  }
